f4ba4dae4da77692633fd391cb508f3d4c46570c
llorracc <git@llorracc.org>
2024-10-05 18:12:18 -0400

    bundledoc worked!

---
+++ bundled/reproduce_text_BufferStockTheory.sh
@@ -0,0 +1,1 @@
+/Volumes/Data/Papers/BufferStockTheory/BufferStockTheory-make/makePDF-Portable.sh /Volumes/Data/Papers/BufferStockTheory/BufferStockTheory-Public BufferStockTheory public bib . Resources
